11bda2370e92771eb44589129bee2ccc4fb29017
Cameron Mullins <cmully@yahoo.com>
2016-09-13 16:34:23 -0700

    updated the Comments Backend Controller

---
+++ server/controllers/comments.js
@@ -0,0 +1,17 @@
+var Answer = mongoose.model('Answer');
+var Topic = mongoose.model('Topic');
+var User = mongoose.model('User');
+
+module.exports = (function(){
+  return{
+    index: function(req, res){
+
+    },
+    show: function(req, res){
+
+    },
+    create: function(req, res){
+
+    }
+  }//close of return
+})();
